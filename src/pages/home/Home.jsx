3335080351fbcb39f6ee5c6c6ea7792b84d29560
BaoDoktah <kimgiabao3333@gmail.com>
2023-10-25 14:05:30 +0700

    Your commit message

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../../components/navbar/Navbar";
+import Carousel from "../../components/carousel/Carousel";
+
+
+function Home() {
+    return (
+        <>
+        <Navbar />
+        <Carousel />
+        </>
+    );
+}
+
+export default Home;
