93b445fd5db4578e8092ff62611a2bad2c49eb97
BaoDoktah <kimgiabao3333@gmail.com>
2023-10-27 18:07:37 +0700

    Them navbar log cac kieu

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,14 +6,14 @@ export default function Card() {
   return (
     <div className="card">
     <div className="ctxh">
-        <h1>TÍCH LŨY</h1>
-        <h2>NGÀY CTXH</h2>
+        <h2>TÍCH LŨY</h2>
+        <h1>NGÀY CTXH</h1>
         <p>Tìm kiếm và tham gia các sự kiện, cộng tác hỗ trợ do văn phòng Đoàn, Hội,... tổ chức để tích lũy ngày CTXH.</p>
         <button ><EventIcon/> Đi đến CTXH </button>
     </div>
     <div className="study">
-        <h1>THAM GIA</h1>
-        <h2>NHÓM MÔN HỌC</h2>
+        <h2>THAM GIA</h2>
+        <h1>NHÓM MÔN HỌC</h1>
         <p>Tìm kiếm, thành lập và tham gia các nhóm môn học để hoàn thành các bài tập lớn, báo cáo, đồ án,... dễ dàng hơn bao giờ hết.</p>
         <button ><SchoolIcon/> Đi đến nhóm môn học</button>
     </div>
--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -33,21 +33,22 @@ function SampleNextArrow(props) {
      return (
          <Slider {...settings}>
              <div>
-                 <h1>Welcome to</h1>
-                 <h2>ADVENTURE BOARD</h2>
+                 <h2>Welcome to</h2>
+                 <h1>ADVENTURE BOARD</h1>
                  <p>Tìm kiếm và tham gia các sự để kiện tích lũy ngày CTXH, hoặc kết nối với sinh viên khác để lập nhóm môn học.</p>
                  <button > <Link to="logged"> Bắt đầu <i class="fas fa-arrow-right"></i> </Link></button>
              </div>
 
              <div>
-                 <h1>TÍCH LŨY</h1>
-                 <h2>NGÀY CTXH</h2>
+                 <h2>Tích lũy</h2>
+                 <h1>NGÀY CTXH</h1>
                  <p>Tìm kiếm và tham gia các sự kiện, cộng tác hỗ trợ do văn phòng Đoàn, Hội,... tổ chức để tích lũy ngày CTXH.</p>
                  <button> <Link to="logged">Bắt đầu <i class="fas fa-arrow-right"></i> </Link></button>
              </div>
+             
              <div>
-                 <h1>THAM GIA</h1>
-                 <h2>NHÓM MÔN HỌC</h2>
+                 <h2>Tham gia</h2>
+                 <h1>NHÓM MÔN HỌC</h1>
                  <p>Tìm kiếm, thành lập và tham gia các nhóm môn học để hoàn thành các bài tập lớn, báo cáo, đồ án,... dễ dàng hơn bao giờ hết.</p>
                  <button > <Link to="logged">Bắt đầu <i class="fas fa-arrow-right"></i> </Link></button>
              </div>
