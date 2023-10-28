0f30d2c9ea45b305b89511cc83e4c853465fdee4
BaoDoktah <kimgiabao3333@gmail.com>
2023-10-28 11:38:45 +0700

    Them login cac kieu

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -9,13 +9,13 @@ export default function Card() {
         <h2>TÍCH LŨY</h2>
         <h1>NGÀY CTXH</h1>
         <p>Tìm kiếm và tham gia các sự kiện, cộng tác hỗ trợ do văn phòng Đoàn, Hội,... tổ chức để tích lũy ngày CTXH.</p>
-        <button ><EventIcon/> Đi đến CTXH </button>
+        <button><EventIcon /> Đi đến CTXH </button>
     </div>
     <div className="study">
         <h2>THAM GIA</h2>
         <h1>NHÓM MÔN HỌC</h1>
         <p>Tìm kiếm, thành lập và tham gia các nhóm môn học để hoàn thành các bài tập lớn, báo cáo, đồ án,... dễ dàng hơn bao giờ hết.</p>
-        <button ><SchoolIcon/> Đi đến nhóm môn học</button>
+        <button><SchoolIcon/> Đi đến nhóm môn học</button>
     </div>
     </div>
   )
