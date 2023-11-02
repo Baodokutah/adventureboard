753b037fbd817b0804cdc2514087e37045b7d2f6
nyankirie <nyankirie@gmail.com>
2023-11-02 19:27:08 +0700

    pepedump

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import './post.css';
 
 function InPost({ title, tags, content}) {
@@ -12,9 +13,28 @@ function InPost({ title, tags, content}) {
           </div>
         ))}
       </div>
-      <pre className='content'>{content}</pre>
-      <div className='comment'>
+      <div className='content'><pre>{content}</pre></div>
+      <div className='cmt'>
         <input className='commentInput'></input>
+
+        <div className="ui comments">
+          <div className="comment">
+            <div className="avatar">
+              <img src="https://media.discordapp.net/attachments/1067775825048510534/1084508310272737420/IMG_0260.jpg?ex=6550f892&is=653e8392&hm=1b9ba1880c4a665705a7908585f3cdddbfda9683f832cf94d51e1e0ef7b326d9&=&width=447&height=662" />
+            </div>
+            <div className="content">
+              <a className="author">Kao Gia Bỉm</a>
+              <div className="metadata">
+                <div>Today at 5:42PM</div>
+              </div>
+              <div className="text">Cho em hỏi mình làm cả 2 ca thì được tổng là 1 ngày CTXH đúng không ạ?</div>
+              <div className="actions">
+                <a className="">Trả lời</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   );
