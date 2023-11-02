258b0ebebf94e7fa70e8df97c47c343660845864
BaoDoktah <kimgiabao3333@gmail.com>
2023-11-02 20:22:14 +0700

    Lmao2

---
+++ src/pages/ctxh/Ctxh.jsx
@@ -0,0 +1,19 @@
+// import Navbar from "../../components/navbar/Navbar";
+import FilterBox from '../../components/filter/Filter';
+import Posts from '../../components/post/Post';
+import ListOfMem from '../../components/listofmem/Listofmem';
+
+import './ctxh.css'
+
+function CTXH() {
+    return (
+        <div className='componentDisplay'>
+            {/* <FilterBox /> */}
+            {/* <Posts /> */}
+            {/* <ListOfMem />
+            <Posts /> */}
+        </div>
+    );
+}
+
+export default CTXH;
