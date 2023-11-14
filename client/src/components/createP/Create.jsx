5a2de701c36dacf7fc641b43d09dfa190f08fd2f
nyankirie <nyankirie@gmail.com>
2023-11-14 23:30:25 +0700

    add tag pick pop up, add json tag

--- client/src/components/createP/Create.jsx
+++ client/src/components/createP/Create.jsx
@@ -1,21 +1,35 @@
 import './create.css'
-import PopupButton from '../popup/Popup';
+import {Confirm, Success, Filter, newTags} from '../popup/Popup';
 import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 function Create()
 {
     const [openModal, setOpenModal] = useState(false);
+    const [openFilter, setOpenFilter] = useState(false);
     const location = useLocation();
-    const currentPage = localStorage.getItem('currentPage') || '404';    
-
+    const currentPage = localStorage.getItem('currentPage') || '404';
     return(
         <div className="create">
             <div className='createInput'>
                 <input placeholder="Tiêu đề" className='titleInput'></input>
                 <input placeholder="Nội dung (không bắt buộc)" className='contentInput'></input>
             </div>
-            <button className='addTagButton'>+</button>
+            <div style={{display:"flex", flexDirection:"row", gap:'1.7%'}}>
+                {newTags.map((tags, index) => (
+                    <div key={index}  className='tag' style={{width: "7.88%", height: "3.36%"}}>
+                        {tags}
+                    </div>
+                ))}
+                <button onClick={() => setOpenFilter(true)} className='addTagButton'>+</button>
+                <Filter
+                open={openFilter}
+                onClose={() => setOpenFilter(false)}
+                page={currentPage}
+                />
+            </div>
+
+
             <div className='quantity'>Số lượng: <input className='quantityInput'></input></div>
             {/* <button className='postButton' onClick={PopupButton}>Đăng</button> */}
             {/* <PopupButton csstyle='postButton' buttonName='Đăng' action='đăng bài viết'/> */}
@@ -23,13 +37,15 @@ function Create()
                 <button onClick={() => setOpenModal(true)} className='postButton'>
                     Đăng
                 </button>
-                <PopupButton
+                <Success
                 open={openModal}
                 onClose={() => setOpenModal(false)}
-                action='Tạo bài viết thành công!' 
+                action='Tạo bài viết thành công!'
                 page={currentPage}
+                imgSrc={"https://www.svgrepo.com/show/522783/check-circle.svg"}
                 />
             </div>
+        <Filter/>
         </div>
     );
 }
