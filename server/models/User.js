b9c3b2bb247d4d62b916219b2d774ec700300849
Lio <bentnt1982@gmail.com>
2023-11-15 23:45:55 +0700

    insert and query

--- server/models/User.js
+++ server/models/User.js
@@ -1,27 +1,17 @@
 const mongoose = require('mongoose');
-const autoIncrement = require('mongoose-auto-increment');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-const db = mongoose.connection;
-// Initialize the auto-increment plugin
-autoIncrement.initialize(db);
-
 const UserSchema = new mongoose.Schema({
-  _id: {
-    type: Number,
-    required: true,
-    unique: true
-  },
   name: {
     type: String,
     required: true,
     min: 3,
     max: 50,
-    unique: true
   },
   mail: {
     type: String,
@@ -35,6 +25,6 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-UserSchema.plugin(autoIncrement.plugin, 'User');
+UserSchema.plugin(AutoIncrement, { inc_field: 'uid' });
 
 module.exports = mongoose.model("User", UserSchema);
